ca0501ecf7d56caf5d6a2e662d0ccc183d6290c7
vgee-hui <44604172+vgee-oliver@users.noreply.github.com>
2018-10-30 22:11:33 +0800

    服务

---
+++ assets/scripts/index.js
@@ -0,0 +1,26 @@
+
+/*第一屏轮播*/
+layui.use('element', function () {
+    var element = layui.element;
+});
+layui.use('carousel', function () {
+    var carousel = layui.carousel;
+    carousel.render({
+        elem: '#header-content',
+        width: '100%',
+        height: '500px',
+        arrow: 'none',
+        interval: 6000,
+        anim: 'fade',
+        indicator: 'none'
+    });
+});
+
+/*导航栏下拉变白*/
+$(function() {
+    $(window).scroll(function () {
+        $(window).scrollTop() > 200 ? $("#menu").addClass("scroll") : $("#menu").removeClass("scroll");
+        $(window).scrollTop() > 200 ? $("#header-logo").addClass("black") : $("#header-logo").removeClass("black");
+        $(window).scrollTop() > 200 ? $(".a").addClass("nav") : $(".a").removeClass("nav");
+    });
+});
